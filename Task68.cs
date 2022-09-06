6e63e8239d7f97d4ca742bfd09d2baa618d3d9b9
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-09-06 22:54:47 +0300

    Task68

---
+++ Task68.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число m: ");
+ulong m = Convert.ToUInt64(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+ulong n = Convert.ToUInt64(Console.ReadLine());
+ulong Acc(ulong M, ulong N) {
+    if (M == 0) {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0) {
+        return Acc(M - 1, 1);
+    }
+    else {
+        return Acc(M - 1, Acc(M, N - 1));
+    }
+}
+Console.WriteLine(Acc(m, n));
